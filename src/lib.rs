cc926397d7066720394e707a4ed73f083e7f3ed3
likai <1175474595@qq.com>
2024-02-02 15:56:22 +0800

    fix: return cstr

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ use anyhow::{anyhow, Result};
 use core::slice;
 use finutils::txn_builder::TransactionBuilder;
 use finutils::txn_builder::TransferOperationBuilder;
-use ledger::data_model::{TxoRef, TxoSID, Utxo, ASSET_TYPE_FRA, b64dec};
+use globutils::wallet;
+use ledger::data_model::{b64dec, TxoRef, TxoSID, Utxo, ASSET_TYPE_FRA};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
@@ -14,7 +15,6 @@ use zei::serialization::ZeiFromToBytes;
 use zei::xfr::asset_record::{open_blind_asset_record, AssetRecordType};
 use zei::xfr::sig::XfrPublicKey;
 use zei::xfr::structs::{AssetRecordTemplate, OwnerMemo};
-use globutils::wallet;
 
 #[no_mangle]
 pub extern "C" fn add(a: u64, b: u64) -> u64 {
@@ -52,16 +52,11 @@ pub extern "C" fn get_tx_str(
     let tick = unsafe { slice::from_raw_parts(tick_ptr, tick_len as usize) };
     let url = unsafe { slice::from_raw_parts(url_ptr, url_len as usize) };
     let url_str = std::str::from_utf8(url).unwrap();
-    let to_pub_str = std::str::from_utf8(to_pub_key).unwrap();
 
     let from_key_str = std::str::from_utf8(from_key).unwrap();
     let from = wallet::restore_keypair_from_mnemonic_default(from_key_str).unwrap();
-
     let aaaas = b64dec(to_pub_key).unwrap();
     let to = XfrPublicKey::zei_from_bytes(aaaas.as_slice()).unwrap();
-    // let code = AssetTypeCode {
-    //     val: ASSET_TYPE_FRA,
-    // }.to_base64();
     let asset_record_type = AssetRecordType::from_flags(false, false);
 
     let mut op = TransferOperationBuilder::new();
@@ -69,8 +64,11 @@ pub extern "C" fn get_tx_str(
     // build input
     let mut input_amount = 0;
     let mut t_amout;
-    let mut diff = 0;
-    let utxos = get_owned_utxos_x(url_str, wallet::public_key_to_base64(from.get_pk_ref()).as_str()).unwrap();
+    let utxos = get_owned_utxos_x(
+        url_str,
+        wallet::public_key_to_base64(from.get_pk_ref()).as_str(),
+    )
+    .unwrap();
     for (sid, (utxo, owner_memo)) in utxos.into_iter() {
         let oar = open_blind_asset_record(&utxo.0.record, &owner_memo, &from).unwrap();
         if oar.asset_type != ASSET_TYPE_FRA {
@@ -79,43 +77,45 @@ pub extern "C" fn get_tx_str(
         t_amout = oar.amount;
         input_amount += t_amout;
 
-        if input_amount >= trans_amount {
-            // if input big than trans amount
-            op.add_input(TxoRef::Absolute(sid), oar, None, None, diff).unwrap();
-            break;
-        } else {
-            // if input small than trans amount
-            diff = trans_amount - input_amount;
-            op.add_input(TxoRef::Absolute(sid), oar, None, None, t_amout).unwrap();
+        if t_amout != 0 {
+            op.add_input(TxoRef::Absolute(sid), oar, None, None, t_amout)
+                .unwrap();
+            if input_amount > TX_FEE_MIN {
+                break;
+            }
         }
     }
-
 
     let memo_struct = Memo::new(
         "brc-20".to_string(),
         "transfer".to_string(),
         std::str::from_utf8(tick).unwrap().to_string(),
-        input_amount.to_string(),
+        trans_amount.to_string(),
     );
     let memo = serde_json::to_string(&memo_struct).unwrap();
-    let template = AssetRecordTemplate::with_no_asset_tracing(
-        trans_amount,
-        ASSET_TYPE_FRA,
-        asset_record_type,
-        to,
-    );
+    let template =
+        AssetRecordTemplate::with_no_asset_tracing(0, ASSET_TYPE_FRA, asset_record_type, to);
 
-    // TODO check gas fee to from or BLACK_HOLE_PUBKEY
-    let template_fee = AssetRecordTemplate::with_no_asset_tracing(
-        input_amount - trans_amount,
+    let template_from = AssetRecordTemplate::with_no_asset_tracing(
+        input_amount - TX_FEE_MIN,
         ASSET_TYPE_FRA,
         asset_record_type,
         from.get_pk(),
+    );
+
+    let template_fee = AssetRecordTemplate::with_no_asset_tracing(
+        TX_FEE_MIN,
+        ASSET_TYPE_FRA,
+        asset_record_type,
+        *BLACK_HOLE_PUBKEY,
     );
     // build output
     let trans_build = op
         .add_output(&template_fee, None, None, None, None)
         .and_then(|b| b.add_output(&template, None, None, None, Some(memo)))
+        .and_then(|b| b.add_output(&template_from, None, None, None, None))
+        .and_then(|b| b.create(TransferType::Standard))
+        .and_then(|b| b.sign(&from))
         .and_then(|b| b.transaction())
         .unwrap();
 
@@ -129,7 +129,7 @@ pub extern "C" fn get_tx_str(
 
     let tx_str = serde_json::to_string(&tx).unwrap();
     let c_string = CString::new(tx_str).unwrap();
-    c_string.as_ptr() as *const c_char
+    c_string.into_raw()
 }
 
 #[no_mangle]
@@ -169,7 +169,6 @@ fn get_owned_utxos_x(
         })
 }
 
-
 #[cfg(test)]
 mod tests {
     use crate::{add, Memo};
@@ -181,9 +180,17 @@ mod tests {
 
     #[test]
     fn test_memo() {
-        let memo_struct = Memo::new("brc-20".to_string(), "transfer".to_string(), "ordi".to_string(), "1000".to_string());
+        let memo_struct = Memo::new(
+            "brc-20".to_string(),
+            "transfer".to_string(),
+            "ordi".to_string(),
+            "1000".to_string(),
+        );
         let memo = serde_json::to_string(&memo_struct).unwrap();
         println!("{}", memo);
-        assert_eq!("{\"p\":\"brc-20\",\"op\":\"transfer\",\"tick\":\"ordi\",\"amt\":\"1000\"}", memo)
+        assert_eq!(
+            "{\"p\":\"brc-20\",\"op\":\"transfer\",\"tick\":\"ordi\",\"amt\":\"1000\"}",
+            memo
+        )
     }
 }
